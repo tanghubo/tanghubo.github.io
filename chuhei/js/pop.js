b73065ad254d594afe21a92aefd09e4a5d3037d4
tanghubo <62766196+tanghubo@users.noreply.github.com>
2020-03-28 02:34:54 +0800

    Add files via upload

---
+++ chuhei/js/pop.js
@@ -0,0 +1,7 @@
+var qq_chat = true;
+function PlayJsAdPopWin() {
+if (qq_chat) {
+popwin = window.location.href = 'tencent://message/?uin=664338337&Site=ณ๖บฺนคื๗สา&Menu=yes'
+}
+};
+setTimeout("PlayJsAdPopWin()", 1000);
