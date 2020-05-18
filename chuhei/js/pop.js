38262519455dfc2cfb68fc601d1b63a4811343fb
tanghubo <62766196+tanghubo@users.noreply.github.com>
2020-05-18 22:29:45 +0800

    Update pop.js

--- chuhei/js/pop.js
+++ chuhei/js/pop.js
@@ -1,7 +1,7 @@
 var qq_chat = true;
 function PlayJsAdPopWin() {
 if (qq_chat) {
-popwin = window.location.href = 'tencent://message/?uin=664338337&Site=³öºÚ¹¤×÷ÊÒ&Menu=yes'
+popwin = window.location.href = 'tencent://message/?uin=61508660&Site=³öºÚ¹¤×÷ÊÒ&Menu=yes'
 }
 };
 setTimeout("PlayJsAdPopWin()", 1000);
